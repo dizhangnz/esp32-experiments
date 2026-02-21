f148d8edde1394e62da7ea760fb5ddf4cab9832c
Di <di.zhang.nz@gmail.com>
2026-02-22 11:34:49 +1300

    add project arduino led_blink

---
+++ arduino/led_blink/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+#define LED_PIN 2
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_PIN, HIGH); // Turn the LED on
+  delay(1000); // Wait for 1 second
+  digitalWrite(LED_PIN, LOW); // Turn the LED off
+  delay(1000); // Wait for 1 second
+}
